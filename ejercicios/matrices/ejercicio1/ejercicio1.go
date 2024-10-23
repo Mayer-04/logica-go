def798774ce5350828acb0e968bfc3aa1ef3a998
Mayer-04 <andres.20.chaves@gmail.com>
2024-10-23 15:21:44 -0500

    add: nuevas formas de resolver el ejercicio

--- ejercicios/matrices/ejercicio1/ejercicio1.go
+++ ejercicios/matrices/ejercicio1/ejercicio1.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 /*
-Dada una matriz cuadrada (3x3), calcula la diferencia `absoluta` entre las sumas de sus diagonales.
+Dada una matriz (3x3), calcula la diferencia `absoluta` entre las sumas de sus diagonales.
+
+CONSIDERACIONES:
+- La diferencia absoluta entre dos números es siempre positiva o cero.
+- Se calcula como: |a - b|, es decir, si `a` es mayor o menor que `b`, el resultado siempre es positivo.
+- Si el número es negativo, se convierte en positivo, y si ya es positivo o es cero, se queda igual.
+- Multiplicar un número negativo por -1 siempre resulta en un número positivo.
 
 Por ejemplo, la matriz cuadrada se muestra a continuación:
 
@@ -15,32 +23,68 @@ Por ejemplo, la matriz cuadrada se muestra a continuación:
 - La diagonal de derecha a izquierda = 3 + 5 + 9 = 17.
 - Su diferencia absoluta es = |15 - 17| = 2.
 
-[][]int: una matriz de números enteros
-Devolvera la diferencia absoluta entre las sumas de sus diagonales
+Entrada:
+[][]int: Es la matriz de números enteros.
+Salida:
+Devolver la diferencia absoluta entre las sumas de sus diagonales.
 */
 
 func main() {
 	arr := [][]int{
-		{11, 2, 4},
+		{1, 2, 3},
 		{4, 5, 6},
-		{10, 8, -12},
+		{9, 8, 9},
 	}
 
-	fmt.Println(diagonalDifference(arr))
+	fmt.Println(diagonalDifference(arr))  // Output: 2
+	fmt.Println(diagonalDifference2(arr)) // Output: 2
+	fmt.Println(diagonalDifference3(arr)) // Output: 2
 }
 
 func diagonalDifference(arr [][]int) int {
-	var suma1 int
-	var suma2 int
-	var result int
+	sumaIzq := 0
+	sumaDer := 0
+
 	for i := 0; i < len(arr); i++ {
-		suma1 += arr[i][i]
-		suma2 += arr[i][len(arr)-1-i]
-		if suma1 > suma2 {
-			result = suma1 - suma2
-		} else {
-			result = suma2 - suma1
-		}
+		sumaIzq += arr[i][i]
+		sumaDer += arr[i][len(arr)-1-i]
 	}
-	return result
+
+	// absoluta := int(math.Abs(float64(sumaIzq - sumaDer)))
+	if sumaIzq > sumaDer {
+		return sumaIzq - sumaDer
+	}
+	return sumaDer - sumaIzq
+}
+
+func diagonalDifference2(arr [][]int) int {
+	sumaIzq := 0
+	sumaDer := 0
+
+	for i, fila := range arr {
+		sumaIzq += fila[i]
+		sumaDer += fila[len(arr)-1-i]
+	}
+
+	sumaTotal := sumaIzq - sumaDer
+	if sumaTotal < 0 {
+		sumaTotal *= -1
+	}
+	return sumaTotal
+}
+
+func diagonalDifference3(arr [][]int) int {
+	sumaIzq := 0
+	sumaDer := 0
+
+	for i, fila := range arr {
+		sumaIzq += fila[i]
+		sumaDer += fila[len(arr)-1-i]
+	}
+
+	sumaTotal := sumaIzq - sumaDer
+	if sumaTotal < 0 {
+		sumaTotal = -sumaTotal // -(-2) = 2
+	}
+	return sumaTotal
 }
