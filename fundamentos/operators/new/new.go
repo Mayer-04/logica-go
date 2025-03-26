0f29db8b44e3cc88c32fcb30262f4c269ac0bd51
Mayer-04 <andres.20.chaves@gmail.com>
2025-03-26 12:41:16 -0500

    add: comentarios adicionales con explicaciones

--- fundamentos/operators/new/new.go
+++ fundamentos/operators/new/new.go
@@ -4,9 +4,17 @@ import "fmt"
 
 /*
 * Operador new
-- La función new(T) se utiliza para crear una nueva instancia de un tipo T, devolviendo un puntero al tipo T.
+- La función `new(T)` se utiliza para crear una nueva instancia de un tipo `T`, devolviendo un puntero al tipo `T`.
 - Esto es útil cuando quieres inicializar una variable y trabajar directamente con su dirección en memoria.
 - Cuando se busca maximizar el rendimiento se recomiendautilizar new(T) en vez de &T{}.
+- Se usa principalmente con tipos no compuestos como int, float64 o bool.
+- El operador `&` (ampersand) es más común cuando se trabaja con tipos compuestos como structs.
+
+* IMPORTANTE
+Si el puntero creado con `new()` solo se usa dentro de una función y no "escapa" a otras partes del programa,
+Go puede asignarlo en la pila (stack) en lugar del heap.
+- Esto hace que la operación sea más rápida y eficiente, ya que la memoria en la pila se libera
+automáticamente cuando la función termina.
 */
 
 type Person struct {
@@ -25,6 +33,15 @@ func main() {
 
 	// Asignar un valor a la dirección de memoria apuntada por x.
 	*x = 10
+
+	//* Ejemplo 2
+	var a *int
+	// Asigna memoria para un entero e inicializa el valor con 0 (valor cero por defecto de int).
+	var b *int = new(int)
+
+	fmt.Println(a)  // nil
+	fmt.Println(b)  // 0xc00000a120
+	fmt.Println(*b) // 0 (valor por defecto de int)
 
 	// Acceder al valor almacenado en la dirección de memoria usando el operador de desreferenciación (*).
 	fmt.Println("Nuevo valor de x:", *x) // Output: Nuevo valor de x: 10
@@ -50,7 +67,7 @@ func main() {
 	// Verificar si dos punteros creados con new apuntan a diferentes ubicaciones de memoria.
 	fmt.Println("¿Apuntan a la misma dirección de memoria?", person == otraPersona) // Output: false
 
-	// Uso de new con tipos básicos como `float64`.
+	// Uso de new() con tipos básicos como `float64`.
 	var pi = new(float64)
 	*pi = 3.14159
 	// Output: Valor de pi: 3.14159, Dirección de memoria: 0xc00000c0a8
