ca785f975dfdd391e5f5f00c3a5389bfe6e2d133
Mayer-04 <andres.20.chaves@gmail.com>
2025-04-06 02:25:17 -0500

    add: nuevas definiciones

--- fundamentos/tiposdatos/string/string.go
+++ fundamentos/tiposdatos/string/string.go
@@ -8,32 +8,67 @@ import (
 )
 
 /*
-* Strings en Go: Secuencia de bytes inmutables.
+* Strings en Go: Secuencia de bytes.
 - Los strings en Go son inmutables y codificados en UTF-8.
+- Para definir un string, utilizamos comillas dobles ("").
+- Caracteres especiales (saltos de línea, tabulaciones y espacios) forman parte del string.
 - Se almacenan como una secuencia de bytes, pero no son equivalentes a `[]byte`.
 - Un carácter ASCII ocupa 1 byte, mientras que los caracteres Unicode pueden ocupar entre 1 y 4 bytes en UTF-8.
-- Los strings se definen con comillas dobles ("").
-- Caracteres especiales (saltos de línea, tabulaciones y espacios) forman parte del string.
 - Para concatenar, se usa `+`, `fmt.Sprintf()` o `strings.Builder` (más eficiente para múltiples concatenaciones).
 - Los caracteres entre comillas simples ('') son `rune` (alias de `int32`), representando un solo carácter Unicode.
+- Cuando necesitas trabajar con caracteres individuales de una cadena, debes usar el tipo `rune`.
+- Al iterar sobre una cadena usando el bucle `for range`,
+Go automáticamente descompone la cadena en runas (no en bytes).
 
-* Substrings: Son secuencias de caracteres consecutivos que se encuentran dentro del string original.
+* Inmutabilidad:
+Significa que una vez que se crea, no se puede modificar su contenido.
+
+* Substrings:
+Son secuencias de caracteres consecutivos que se encuentran dentro del string original.
 
 * Substrings:
 - Un substring es una secuencia consecutiva de caracteres dentro de un string.
 - Los substrings son inmutables: cualquier modificación genera un nuevo string.
-* Substrings únicos: Son substrings que no se repiten.
 
-* Definiciones clave:
-- Secuencia: Conjunto ordenado de elementos.
-- Byte: Es la unidad básica de almacenamiento de datos en una computadora. Puede representar valores de 0 a 255.
-- Codificación: Es el proceso de transformar datos o información a un formato específico que pueda ser procesado
-o entendido por una máquina o computadora (ej., UTF-8).
-- ASCII: Conjunto de caracteres que usa 7 bits para representar 128 caracteres (0-127),
-incluyendo letras, números y signos de puntuación.
-- Unicode: Estándar que asigna un número único (código) a cada carácter en casi todos los idiomas del mundo.
-- UTF-8: Es un tipo de codificación que se usa para representar caracteres de texto en una computadora.
-Puede usar entre 1 y 4 bytes para representar cada carácter.
+* Substrings únicos:
+Son substrings que no se repiten.
+
+* DEFINICIONES CLAVE 📖:
+
+* Secuencia:
+Conjunto ordenado de elementos.
+
+* Byte:
+Es la unidad básica de almacenamiento de datos en una computadora. Puede representar valores de 0 a 255 (8 bits).
+
+* Codificación:
+Es el proceso de `transformar` datos o información a un formato específico que pueda ser procesado
+o entendido por una máquina o computadora (como UTF-8).
+
+* ASCII:
+Conjunto de caracteres que usa `7 bits` para representar 128 caracteres (0-127),
+incluyendo letras del alfabeto inglés, números y signos de puntuación.
+- Es un subconjunto de UTF-8.
+
+* Unicode:
+Estándar internacional que asigna un número único (punto de código) a cada carácter en casi todos los idiomas
+del mundo, símbolos matemáticos, emojis y otros símbolos.
+
+* Punto de código:
+Valor numérico que representa un carácter específico en Unicode.
+Por ejemplo, la letra "A" es el punto de código U+0041.
+
+* UTF-8:
+Es un tipo de codificación que se usa para representar caracteres de texto (Unicode) en una computadora.
+Puede usar entre 1 y 4 bytes por carácter: 1 byte para caracteres ASCII y más bytes para otros caracteres.
+- UTF-8 simplemente extiende ASCII para incluir caracteres adicionales.
+- En UTF-8, los códigos de 0 a 127 son exactamente los mismos que en ASCII.
+
+* UTF-16:
+Es una codificación de Unicode que usa 2 bytes por carácter.
+- Usa mínimo 2 bytes para cada carácter, incluso para caracteres ASCII simples.
+- Es más eficiente con idiomas que utilizan muchos caracteres como (japonés, coreano o chino).
+
 */
 
 func main() {
@@ -70,7 +105,7 @@ func main() {
 	fmt.Println("concatenar strings:", nameAndSecondName) // Output: "Mayer Andres"
 
 	//* Iterar sobre un string con `range`.
-	// Go no itera sobre los bytes de un string, sino sobre sus runas (puntos de código Unicode).
+	// Go no itera sobre los bytes de un string, sino sobre sus runas.
 	for i, char := range name {
 		fmt.Printf("index: %d, char: %d\n", i, char) // Output: index: 0, char: 77, etc ...
 	}
