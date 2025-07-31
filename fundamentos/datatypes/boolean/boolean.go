2b71486b203efc53d655dd3d5c95cc0d76b38539
Mayer-04 <andres.20.chaves@gmail.com>
2025-07-31 16:40:08 -0500

    add: nuevos comentarios explicativos

--- fundamentos/datatypes/boolean/boolean.go
+++ fundamentos/datatypes/boolean/boolean.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 /*
-* Booleanos en Go (bool)
+* Booleanos en Go
 Un tipo de dato booleano solo puede tener dos valores: `true` (verdadero) o `false` (falso).
 
 ¿Para qué se usan?
-- Representan condiciones lógicas, como "¿está activo?" o "¿es mayor de edad?".
+- Representan condiciones lógicas, como "¿Está activo?" o "¿Es mayor de edad?".
 - Se usan comúnmente para controlar el flujo del programa en estructuras como:
   - if (condicionales)
   - switch (selección múltiple)
@@ -17,9 +17,9 @@ Un tipo de dato booleano solo puede tener dos valores: `true` (verdadero) o `fal
   - `||` (OR): verdadero si al menos una condición es verdadera.
   - `!` (NOT): invierte el valor (true pasa a false y viceversa).
 
-Características del tipo `bool` en Go:
-- Es estricto: no puedes convertir un `bool` a otro tipo como entero o cadena (ni viceversa).
-- Go NO hace conversiones automáticas (implícitas), lo que evita errores inesperados.
+* Características del tipo `bool` en Go:
+- Es estricto: No puedes convertir un `bool` a otro tipo como entero o cadena (ni viceversa).
+- Go NO hace conversiones automáticas, lo que evita errores inesperados.
 - Aunque un booleano solo necesita 1 bit, Go reserva 1 byte (8 bits) por eficiencia,
 ya que los procesadores acceden a la memoria por bytes.
 */
